0c1e2988504526a64f523fbde146a11358c52c3b
jtm029 <43485929+jtm029@users.noreply.github.com>
2020-04-18 01:11:12 -0500

    option6

--- src/app/features/view-given/view-given.component.ts
+++ src/app/features/view-given/view-given.component.ts
@@ -22,7 +22,7 @@ export class ViewGivenComponent implements OnInit {
   selectedStudent: Student;
   selectedEnrollment: Enrollment;
   courses: Courses[];
-  enrollments: Enrollment[];
+  enrollments: Enrollment[] = [];
 
   @Input() set viewComponent(value: boolean){
 
